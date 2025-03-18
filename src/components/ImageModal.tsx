3555e69987749262a4d3e7f0d65cf2336f2129e2
Rishvan <rishvan.m@globalgov.ae>
2025-03-18 08:32:18 +0400

    animation updated

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -27,6 +27,10 @@ const ImageModal: React.FC<ImageModalProps> = ({
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0.8 }}
+        transition={{
+          duration: 0.4,
+          ease: [0.4, 0, 0.2, 1],
+        }}
         className="relative p-4"
         onClick={(e) => e.stopPropagation()}
       >
