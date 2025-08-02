f56b4cac7ebc450c534b89485021352717895c13
Rishvan <rishvan.m@globalgov.ae>
2025-08-02 18:33:06 +0400

    JSON UPDATED

--- src/components/Home/Works.tsx
+++ src/components/Home/Works.tsx
@@ -6,14 +6,16 @@ import Videos from "./Videos";
 import digitalArts from "../../../public/data/digitalArt.json";
 
 export default function Works() {
-  const { sketch, video } = works;
+  const [{ sketch, video }] = works;
 
   return (
     <div className="w-full flex flex-col">
       <div className="p-6 text-2xl font-bold flex text-white  bg-[#B2A4D4] flex-col">
         {"Works"}
       </div>
-       <DigitalArts digitalArt={digitalArts.filter((e) => e.isListed === true)} />
+      <DigitalArts
+        digitalArt={digitalArts.filter((e) => e.isListed === true)}
+      />
       <Sketch sketches={sketch.filter((e) => e.isListed === true)} />
       <Videos videos={video.filter((e) => e.isListed === true)} />
     </div>
